b3ea6accd46b75816a120931db7ad3840e24a2c5
WILLMA <willmirva@gmail.com>
2026-06-22 23:07:12 +0700

    Добавил Transaction BBB Domain готово на данном этапе, дальше буду писать инфрструктуру

--- BigBrotherBank.Domain/Entities/Transaction.cs
+++ BigBrotherBank.Domain/Entities/Transaction.cs
@@ -17,7 +17,15 @@ namespace BigBrotherBank.Domain.Entities
         public TransactionType Type { get; private set; }
         public TransactionState Status { get; private set; }
         public DateTime CreatedAtUtc { get; private set; }
-
+        /// <summary>
+        /// Создание операции депозита
+        /// </summary>
+        /// <param name="walletId">ID кошелька</param>
+        /// <param name="amount">Сумма</param>
+        /// <param name="currency">Валюта</param>
+        /// <exception cref="ArgumentException">ID кошелька не может быть пустым</exception>
+        /// <exception cref="InvalidAmountException">Сумма должна быть больше нуля</exception>
+        /// <exception cref="InvalidCurrencyException">Валюта не может быть пустой</exception>
         public void CreateDeposit(Guid walletId, decimal amount, Currencies currency)
         {
             if (walletId == Guid.Empty)
@@ -39,8 +47,42 @@ namespace BigBrotherBank.Domain.Entities
             CreatedAtUtc = DateTime.UtcNow;
         }
 
+        /// <summary>
+        /// Создание операции перевода
+        /// </summary>
+        /// <param name="fromWalletId">ID кошелька отправителя</param>
+        /// <param name="toWalletId">ID кошелька получателя</param>
+        /// <param name="amount">Сумма</param>
+        /// <param name="currency">Валюта</param>
+        /// <exception cref="ArgumentException">ID кошелька не может быть пустым</exception>
+        /// <exception cref="SameWalletTransferException">ID кошельков не могут совпадать</exception>
+        /// <exception cref="InvalidAmountException">Сумма должна быть больше нуля</exception>
+        /// <exception cref="InvalidCurrencyException">Валюта не может быть пустой</exception>
+        public void CreateTransfer(Guid fromWalletId, Guid toWalletId, decimal amount, Currencies currency)
+        {
+            if (toWalletId == Guid.Empty)
+                throw new ArgumentException("Wallet address id cannot be empty.", nameof(toWalletId));
 
+            if (fromWalletId == Guid.Empty)
+                throw new ArgumentException("Wallet sender id cannot be empty.", nameof(fromWalletId));
 
-        //CreateTransfer
+            if (toWalletId == fromWalletId)
+                throw new SameWalletTransferException("Sender and receiver wallets cannot be the same.");
+
+            if (amount <= 0)
+                throw new InvalidAmountException("Amount must be greater than zero.");
+
+            if (currency == Currencies.NONE)
+                throw new InvalidCurrencyException("Currency cannot be empty.");
+
+            Id = Guid.NewGuid();
+            FromWalletId = fromWalletId;
+            ToWalletId = toWalletId;
+            Amount = amount;
+            Currency = currency;
+            Type = TransactionType.Transfer;
+            Status = TransactionState.Completed; //возможно изменю на ожидание когда будет финансовый шлюз
+            CreatedAtUtc = DateTime.UtcNow;
+        }
     }
 }
